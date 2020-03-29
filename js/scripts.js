70de04409a314008f3959182d0b8c57e3afd3183
butorol <oleksandr.butor@gmail.com>
2020-03-29 19:20:58 +0300

    [ SLIDER ] Blog page Slick slider added

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ jQuery(document).ready(function($) {
 	
 	burger_menu();
 
+	/* Blog psge */
+	trending_posts_blog_slider()
+
 });
 
 function burger_menu() {
@@ -15,5 +18,39 @@ function burger_menu() {
 		}
 
 	});
+
+}
+
+function trending_posts_blog_slider() {
+
+	$( '.slider' ).slick({
+		// infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		/*centerMode: true,
+		centerPadding: '60px',*/
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+			}
+		}
+	] });
 
 }
